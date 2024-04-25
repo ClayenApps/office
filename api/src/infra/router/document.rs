1d48abdfd79211324da6f8f0a46d7f626d2e38d5
ClayenKitten <dima070704@yandex.ru>
2024-04-25 03:08:33 +0300

    document routes

--- api/src/infra/router/document.rs
+++ api/src/infra/router/document.rs
@@ -1,5 +1,18 @@
-use axum::Router;
+use axum::{
+    extract::Path, routing::{get, post}, Router
+};
 
 pub fn router() -> Router {
-    Router::new()
+    Router::new().route("/", post(post_document)).route(
+        "/:id",
+        get(get_document).put(put_document).delete(delete_document),
+    )
 }
+
+async fn get_document(Path(id): Path<u32>) {}
+
+async fn post_document() {}
+
+async fn put_document(Path(id): Path<u32>) {}
+
+async fn delete_document(Path(id): Path<u32>) {}
